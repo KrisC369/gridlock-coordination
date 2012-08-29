974e89b8d7582731a4d10779309ff7cf383319ce
Kristof Coninx <kristofconinx369@gmail.com>
2012-08-29 23:19:35 +0200

    Removing Commentary templateheader from test suite source.

--- src/test/java/be/kuleuven/cs/gridlock/coordination/CoordinationSuite.java
+++ src/test/java/be/kuleuven/cs/gridlock/coordination/CoordinationSuite.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package be.kuleuven.cs.gridlock.coordination;
 
 import org.junit.runner.RunWith;
